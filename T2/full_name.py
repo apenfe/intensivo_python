cfa933f470dc2f68278dec02a164f81f280fc736
apenfe <adrian1414@hotmail.es>
2024-05-13 23:15:01 +0200

    variables en cadenas

---
+++ T2/full_name.py
@@ -0,0 +1,9 @@
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
---
+++ T2/name.py
@@ -0,0 +1,5 @@
+nombre= 'ada lovelace'
+
+print(nombre.title())
+print(nombre.upper())
+print(nombre.lower())
