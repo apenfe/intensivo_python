8ff306c18b3279c27814ac7c63965e99c31231cd
apenfe <adrian1414@hotmail.es>
2024-05-15 10:43:38 +0200

    Introduccion a listas

---
+++ T3/bicycles.py
@@ -0,0 +1,13 @@
+bicycles = ['trek', 'cannondale', 'redline', "specialized"]
+
+print(bicycles)
+
+print(bicycles[0])
+print(bicycles[0].title())
+
+print(bicycles[2])
+print(bicycles[3])
+
+print(bicycles[-1])
+
+print(f"Mi primera bici fue una {bicycles[-1]}")
