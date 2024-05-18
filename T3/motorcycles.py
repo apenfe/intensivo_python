9f6483d5c3fe831c6b31b7708e6a0cc62a8e609d
apenfe <adrian1414@hotmail.es>
2024-05-18 21:34:06 +0200

    listas dinamicas

--- T3/motorcycles.py
+++ T3/motorcycles.py
@@ -13,4 +13,20 @@ motos =[]
 motos.append('ducati')
 motos.append('honda')
 motos.append('yamaha')
+
+motos.insert(0,'tenere 700')
+
+del motos[-1] # elimina un elemento de la lista, en este caso borro el ultimo elemento
+print(motos)
+
+moto_eliminada = motos.pop()
+print(motos)
+print(moto_eliminada)
+
+print(f"la ultima moto eliminada es {moto_eliminada}")
+
+primera_eliminada = motos.pop(0)
+print(f"Mi actual moto es una {primera_eliminada}")
+
+motos.remove('ducati')
 print(motos)
