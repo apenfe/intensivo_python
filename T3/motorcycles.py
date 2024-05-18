0750a90c224616a64fcd4ad30f20172c280c0b59
apenfe <adrian1414@hotmail.es>
2024-05-18 21:23:00 +0200

    listas dinamicas

---
+++ T3/motorcycles.py
@@ -0,0 +1,16 @@
+motorcycles = ['honda','yamaha','suzuki']
+print (motorcycles)
+
+motorcycles[0] = 'ducati'
+print (motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+# desde 0
+
+motos =[]
+motos.append('ducati')
+motos.append('honda')
+motos.append('yamaha')
+print(motos)
