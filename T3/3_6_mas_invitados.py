b7acfc526772b0cde1bd3e8b5e4c3afbc033268f
apenfe <adrian1414@hotmail.es>
2024-05-18 21:58:11 +0200

    listas dinamicas

---
+++ T3/3_6_mas_invitados.py
@@ -0,0 +1,16 @@
+personajes = ['cristobal colon']
+personajes.append('Juan Carlos')
+print(f"como fue el descubimiento de america, {personajes[0]}?")
+print(f"como se vive en EAU, {personajes[1]}?")
+print()
+
+personajes.insert(0,'juan')
+personajes.insert(2,'luis')
+personajes.insert(-1,'paco')
+
+print(f"Invitado a la cena, {personajes[0]}?")
+print(f"Invitado a la cena, {personajes[1]}?")
+print(f"Invitado a la cena, {personajes[2]}?")
+print(f"Invitado a la cena, {personajes[3]}?")
+print(f"Invitado a la cena, {personajes[4]}?")
+print()
