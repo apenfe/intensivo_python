dacbe9c65db5ed9e401c008f84efe96d95704ca5
apenfe <adrian1414@hotmail.es>
2024-06-01 19:56:35 +0200

    comienzo bucles

--- T4/First_numbers.py
+++ T4/First_numbers.py
@@ -5,3 +5,10 @@ print("\n")
 
 for value in range(1,6):
     print(value)
+
+print("\n")
+for value in range(6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
---
+++ T4/even_numbers.py
@@ -0,0 +1,2 @@
+pares= list(range(2,11,2))
+print(pares)
