07b2ab565ea21e57fcd53f67ce8a84bad3409020
apenfe <adrian1414@hotmail.es>
2024-06-01 22:18:12 +0200

    comienzo bucles

---
+++ T4/4_7_treses.py
@@ -0,0 +1,5 @@
+# Crear una lista con los múltiplos de 3 entre 3 y 30
+multiplos_de_3 = [i for i in range(3, 31) if i % 3 == 0]
+
+# Imprimir la lista resultante
+print(multiplos_de_3)
---
+++ T4/4_8_cubos.py
@@ -0,0 +1,6 @@
+cubos = []
+
+for i in range(1,11):
+    cubos.append(i**3)
+
+print(cubos)
