6f78d92a2d03165fb5253dcc3200c91f7a92f63d
apenfe <adrian1414@hotmail.es>
2024-06-04 16:21:20 +0200

    Comienzo diccionarios python

---
+++ T6/alien.py
@@ -0,0 +1,19 @@
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+alien_0['points']=10
+
+print(f"Acabas de ganar {alien_0['points']} puntos")
+print (alien_0)
+
+alien_0['x_position']=0
+alien_0['y_position']=25
+print(alien_0)
+
+alien_1 = {}
+
+alien_1['color']='blue'
+alien_1['points']=5
+print(alien_1)
