f4cef81257309f5694db4192ffb25edd092018ae
apenfe <adrian1414@hotmail.es>
2024-06-04 16:29:12 +0200

    Comienzo diccionarios python

--- T6/alien.py
+++ T6/alien.py
@@ -17,3 +17,22 @@ alien_1 = {}
 alien_1['color']='blue'
 alien_1['points']=5
 print(alien_1)
+
+# cambio de color
+alien_1['color']='amarillo'
+print(alien_1)
+
+alien_2 = {'x_position':0, 'y_position':25,'speed':'medium'}
+print(f"Posicion original {alien_2['x_position']}")
+
+# se mueve al alien hacia la derecha basandos e en velocidad actual
+
+if alien_2['speed'] == 'low':
+    x_position = 1
+elif alien_2['speed'] == 'medium':
+    x_position=2
+else:
+    x_position=3
+
+alien_2['x_position']+=x_position
+print(f"Posicion actual {alien_2['x_position']}")
