370bcfd1ade605bf01d12f463efebb52558ee4ca
apenfe <adrian1414@hotmail.es>
2024-06-20 18:18:01 +0200

    dados

---
+++ T15/dados/15_7_tres_dados.py
@@ -0,0 +1,34 @@
+from die import Die
+numero = 6
+veces = 1000
+
+die_1 = Die(numero)
+die_2 = Die(numero)
+die_3 = Die(numero)
+
+results = []
+
+for roll_number in range(veces):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
+    results.append(result)
+
+frecuencies = []
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+poss_results = range(2,max_result+ 1)
+
+for value in poss_results:
+    frequencey = results.count(value)
+    frecuencies.append(frequencey)
+
+print(frecuencies)
+
+import plotly.express as px
+
+title = f"Resultado de lanzar tres dados de {numero} caras {veces} veces"
+labels = {'x':'Resultado', 'y':'Frecuencia'}
+fig = px.bar(x=poss_results, y=frecuencies, title=title, labels=labels)
+
+# añade personalizaciones al grafico
+fig.update_layout(xaxis_dtick=1)
+
+fig.show()
