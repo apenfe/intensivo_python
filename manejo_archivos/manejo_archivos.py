332b83e90a744c8ac5e9716727c0ff78a3b171e4
apenfe <adrian1414@hotmail.es>
2024-06-20 10:58:18 +0200

    manipulacion json, txt y csv

--- manejo_archivos/manejo_archivos.py
+++ manejo_archivos/manejo_archivos.py
@@ -1,5 +1,8 @@
 from urllib.request import urlopen
+from pathlib import Path
 import json
+
+path = Path('datos.json')
 
 respuesta = urlopen("https://gbfs.citibikenyc.com/gbfs/en/station_status.json")
 respuesta = respuesta.read()
@@ -20,3 +23,62 @@ estaciones = json_data['data']['stations']
 print(f'Cantidad de estaciones: {nestaciones}')
 print('\nUn ejemplo de los datos de una estación:')
 print(estaciones[0])
+
+# creamos el fichero de texto
+
+with open('bikesNY.txt', 'w') as archivo:
+    # Crear encabezado
+    archivo.write('NOMBRE EST., BIC. DISPONIBLES, DIR.\n')
+
+    # Procesar cada registro (estación), extraer los campos e incluirla
+    # en el .txt sólo si hay bicicletas disponibles
+    for estacion in estaciones:
+        nombre = estacion['station_id']
+        nbicis = estacion['num_bikes_available']
+        nbicis += estacion['num_ebikes_available']
+        direcc = estacion['legacy_id']
+
+        if nbicis != 0:
+            archivo.write(f'{nombre}, {nbicis}, {direcc}\n')
+
+# Definir formato de columnas:
+# - {0:<35}: columna 0, alineado a la izquierda (<) y ancho de 35 caracteres
+# - {1:>17}: columna 1, alineado a la derecha (>) y ancho de 17 caracteres
+# - {2:>35}: columna 2, alineado a la derecha (>) y ancho de 35 caracteres
+formato_cols = '{0:<35} {1:>17} {2:>35}'
+
+with open('bikesNY_v2.txt', 'w') as archivo:
+    # Crear encabezado
+    archivo.write(formato_cols.format('NOMBRE EST.', 'BIC. DISPONIBLES', 'DIR.\n'))
+
+    # Procesar cada registro (estación), extraer los campos e incluirla
+    #  en el .txt sólo si hay bicicletas disponibles
+    for estacion in estaciones:
+        nombre = estacion['station_id']
+        nbicis = estacion['num_bikes_available']
+        nbicis += estacion['num_ebikes_available']
+        direcc = estacion['legacy_id']
+
+        if nbicis != 0:
+            archivo.write(formato_cols.format(nombre, nbicis, direcc + '\n'))
+
+import csv  # Módulo que hace parte de la librería estándar de Python
+
+with open('bikesNY.csv', 'w') as archivo:
+    writer = csv.writer(archivo)
+
+    # Escribir encabezado: una lista
+    encabezado = ['NOMBRE EST.', 'BIC. DISPONIBLES', 'DIR.']
+    writer.writerow(encabezado)
+
+    # Y escribir fila por fila
+    for estacion in estaciones:
+        # Parse item
+        nombre = estacion['station_id']
+        nbicis = estacion['num_bikes_available']
+        nbicis += estacion['num_ebikes_available']
+        direcc = estacion['legacy_id']
+
+        if nbicis != 0:
+            fila = [nombre, nbicis, direcc]
+            writer.writerow(fila)
