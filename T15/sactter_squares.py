336699a4507fe9bc4766a00ea59980ead29b25d2
apenfe <adrian1414@hotmail.es>
2024-06-20 13:01:50 +0200

    scatter en matplotlib

--- T15/sactter_squares.py
+++ T15/sactter_squares.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 
-input_values = [1,2,3,4,5]
-squares = [1,4,9,16,25]
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 
-# estilo de grafico
 plt.style.use('seaborn-v0_8')
-
 fig, ax = plt.subplots()
+ax.scatter(x_values,y_values,color ='red', s=10)
+# ax.scatter(x_values,y_values,c =(0,0.8,0), s=10)
 
-ax.plot(input_values,squares, linewidth=3)
-
-# establece el titulo del grafico y las etiquetas de los ejes
+#establece el titulo y las etiquetas
 ax.set_title('Square numbers',fontsize=24)
 ax.set_xlabel('Values',fontsize=14)
 ax.set_ylabel('Cuadrado del valor',fontsize=14)
 
 # establece el tamaño de las etiquetas de los puntos de los ejes
 ax.tick_params(labelsize=14)
+
+# establece el rango para cada eje
+
+ax.axis([0,1100,0,1100000])
+ax.ticklabel_format(style='plain')
 
 plt.show()
