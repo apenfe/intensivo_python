185dbb9a242d298ebbfa5bfcf435c49c8a877576
apenfe <adrian1414@hotmail.es>
2024-06-20 13:50:12 +0200

    random walks

--- T15/rw_visual.py
+++ T15/rw_visual.py
@@ -2,13 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-plt.style.use('classic')
-fig, ax = plt.subplots()
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9),dpi=128)
 
-ax.scatter(rw.x_values, rw.y_values, s=1)
-ax.set_aspect('equal')
 
-plt.show()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values,c=point_numbers,cmap=plt.cm.Blues, edgecolors='none', s=5)
+    ax.set_aspect('equal')
+
+    # enfatizar primer y ultimo punto
+    ax.scatter(0,0,c='green',edgecolors='none',s=100)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+
+    # eliminar los ejes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input('Continue? (y/n): ')
+    if keep_running == 'n':
+        break
