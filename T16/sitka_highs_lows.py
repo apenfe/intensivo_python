09d752ae11840c6c7b6c4d2a7f5277f6f85b14ef
apenfe <adrian1414@hotmail.es>
2024-06-21 19:18:07 +0200

    Multiples valores en graficas

--- T16/sitka_highs_lows.py
+++ T16/sitka_highs_lows.py
@@ -10,26 +10,24 @@ reader = csv.reader(lines)
 header_row = next(reader)
 print(header_row)
 
-#for index, column_header in enumerate(header_row):
-#    print(index, column_header)
-
-# extraer temperaturas maximas
-
-dates, highs = [], []
+dates, highs, lows = [], [], []
 
 for row in reader:
     current_date = datetime.strptime(row[2],'%Y-%m-%d')
     high = int(row[5])
+    low = int(row[6])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # trazar temperaturas maximas
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates,highs,color='red')
+ax.plot(dates,lows,color='blue')
 
 # dar formato al trazado
-ax.set_title('Temperaturas maximas 2018', fontsize=20)
+ax.set_title('Temperaturas maximas y minimas 2018', fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperaturas (F)', fontsize=16)
