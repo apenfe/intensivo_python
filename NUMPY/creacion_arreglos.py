67d70e58ae8d691b436a84e607c9bc1880528619
apenfe <adrian1414@hotmail.es>
2024-06-21 17:08:46 +0200

    Numpy primeros pasos

---
+++ NUMPY/creacion_arreglos.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([5,27,2])
+b = np.array([6.2,5.3,8.4,9.2])
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+
+D = np.array([[27,45,33,12],[86,43,52,21]]) # arreglo de 2 x 4
+print(D)
