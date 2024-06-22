74d29cab77a230ce79e3902d0750a525a40ad647
apenfe <adrian1414@hotmail.es>
2024-06-22 20:29:25 +0200

    Llamada API Github

---
+++ T17/java_repos_visual.py
@@ -0,0 +1,36 @@
+import requests
+import plotly.express as px
+
+url = 'https://api.github.com/search/repositories?q=language:java+sort:stars+stars:>10000'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'Status code: {r.status_code}')
+
+response_dict = r.json()
+print(f"Resultados completos: {not response_dict['incomplete_results']}")
+
+# procesa informacion del repositorio
+repo_dicts = response_dict['items']
+repo_links, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    stars.append(repo_dict['stargazers_count'])
+
+    # textos emergentes
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+# crea visualizacion
+title = "Proyectos de Java mejor valorados"
+labels = {'x':"Repositorios",'y':"Estrellas"}
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels,hover_name=hover_texts)
+fig.update_layout(title_font_size=28,xaxis_title_font_size=20,yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue',marker_opacity=0.6)
+fig.show()
